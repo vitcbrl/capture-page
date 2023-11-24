879fdd26e1caba5aa94d4746fe9c93b39788570a
vitoria cabral <vitoria.oliveira@alu.ufc.br>
2023-11-23 22:45:41 -0300

    configurações

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+document.getElementById('cadastroForm').addEventListener('submit', async function(event) {
   event.preventDefault();
 
   // Captura dos dados do formulário
@@ -7,6 +7,12 @@ document.getElementById('cadastroForm').addEventListener('submit', function(even
   const vagas = document.getElementById('vagas').checked;
   const conteudos = document.getElementById('conteudos').checked;
 
+  // Verifica se pelo menos uma opção foi marcada
+  if (!vagas && !conteudos) {
+      console.log('Nenhuma opção foi marcada.');
+      return; // Evita a chamada da API
+  }
+
   // Captura dos parâmetros de URL
   const urlParams = new URLSearchParams(window.location.search);
   const source = urlParams.get('source');
@@ -14,37 +20,38 @@ document.getElementById('cadastroForm').addEventListener('submit', function(even
 
   console.log('Parâmetros de URL - Source:', source, 'Campaign:', campaign);
 
-  // Chamada de API para o ActiveCampaign
-  fetch('https://cassianosavio.api-us1.com/api/3/contacts', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'Api-Token': '30eef11cca1642e6a07bedcd5f6edf65c21065cb6f2dabecda139d4290e05225a99ee7e3'
-      },
-      body: JSON.stringify({
-          contact: {
-              email: email,
-              firstName: nome,
-              field: {
-                  vagas: vagas,
-                  conteudos: conteudos
-              }
+  try {
+      // Chamada de API para o ActiveCampaign
+      const response = await fetch('https://cassianosavio.api-us1.com/api/3/contacts', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Api-Token': '30eef11cca1642e6a07bedcd5f6edf65c21065cb6f2dabecda139d4290e05225a99ee7e3'
           },
-          tags: ['INTERESSADOS MDNEGOCIOS'],
-          lists: ['INTERESSADOS MDNEGOCIOS'],
-          source: source,
-          campaign: campaign
-      })
-  })
-  .then(response => response.json())
-  .then(data => {
+          body: JSON.stringify({
+              contact: {
+                  email: email,
+                  firstName: nome,
+                  field: {
+                      vagas: vagas,
+                      conteudos: conteudos
+                  }
+              },
+              tags: ['INTERESSADOS MDNEGOCIOS'],
+              lists: ['INTERESSADOS MDNEGOCIOS'],
+              source: source,
+              campaign: campaign
+          })
+      });
+
+      const data = await response.json();
+
       console.log('Contato adicionado com sucesso:', data);
       // Exibir mensagem de agradecimento
       document.getElementById('agradecimento').innerText = `Obrigado por se cadastrar, ${nome}! Verifique seu e-mail para confirmação.`;
       document.getElementById('agradecimento').style.display = 'block';
       document.getElementById('cadastroForm').style.display = 'none';
-  })
-  .catch(error => {
+  } catch (error) {
       console.error('Erro ao adicionar contato:', error);
-  });
+  }
 });
